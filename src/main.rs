e4ed0397a944e1b3b9570e84c5c06f2725c9adbe
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-09-02 07:25:32 -0400

    creating an entity with some components

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@
 #[macro_use]
 extern crate ecs;
 
-use ecs::World;
+use ecs::{BuildData,World};
 
-struct Shape;
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+}
 
 components! {
-    struct MyComponents;
+    struct MyComponents {
+        #[hot] position: Position,
+        #[cold] respawn: Position
+    }
 }
 
 systems! {
@@ -18,6 +25,14 @@ fn main() {
     println!("Hello, world!");
     let mut world = World::<MySystems>::new();
 
-    let entity = world.create_entity(());
+    let entity_rm = world.create_entity(());
+    println!("{:?}", entity_rm);
+    world.remove_entity(entity_rm);
+
+    let entity = world.create_entity(|entity: BuildData<MyComponents>, data: &mut MyComponents| {
+        data.position.add(&entity, Position { x : 0.0, y : 0.0 });
+        data.respawn.add(&entity, Position { x : 0.0, y : 0.0 });
+    });
+    assert!(entity_rm != entity);
     println!("{:?}", entity);
 }
